e421b9ca0fd93d582596d2d369f642ef54c09431
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2023-01-26 14:13:38 +0300

    Update

--- internal/handlers/auth/tits.go
+++ internal/handlers/auth/tits.go
@@ -48,7 +48,7 @@ func (h *Handler) tgLogin(w http.ResponseWriter, r *http.Request) {
 		Name:     "boobs_session",
 		Value:    strconv.Itoa(payload.ID),
 		Expires:  expiration,
-		Domain:   ".rate-tits.online",
+		Domain:   "dev.rate-tits.online",
 		Path:     "/",
 		SameSite: http.SameSiteLaxMode,
 		HttpOnly: true,
